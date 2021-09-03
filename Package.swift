d9f86257f88e1a04309cc9d5e62dd9838ea1970a
scandit <scandit@golden.int.mirasense.com>
2021-09-03 18:24:30 +0200

    Updating ScanditCaptureCore - v.6.9.2

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "ScanditCaptureCore", url: "https://ssl.scandit.com/sdk/download/scandit-datacapture-ios-core-6.9.1-xcframework.zip", checksum: "d838ba22bb9b360213038acaae25a72fd13ca0c6c20402f591ee6ae01612bc53")
+        .binaryTarget(name: "ScanditCaptureCore", url: "https://ssl.scandit.com/sdk/download/scandit-datacapture-ios-core-6.9.2-xcframework.zip", checksum: "4aa06016b17eeff1bff0c019d9ec41f5b3d6d0e5885839801f3ba4148d175c8a")
     ]
 )
